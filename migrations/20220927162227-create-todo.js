08e0ebd9db6c6960f9ae6802ee2cc9460e80424e
venkatAkshay <113366014+venkatAkshay@users.noreply.github.com>
2022-12-23 20:20:06 +0530

    Update 20220927162227-create-todo.js

--- migrations/20220927162227-create-todo.js
+++ migrations/20220927162227-create-todo.js
@@ -1,4 +1,4 @@
-'use strict';
+use strict';
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
@@ -32,3 +32,4 @@ module.exports = {
     await queryInterface.dropTable('Todos');
   }
 };
+
